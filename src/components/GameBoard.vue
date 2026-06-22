19cc5826ae8940ff42138ddd4b8ecfa44759e1dc
Rengevich Sergei <ser-rengevich@yandex.ru>
2026-06-22 23:44:35 +0300

    add computed player

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -31,18 +31,29 @@ const board = reactive<BoardItem[]>([
   { player: null, index: 8 },
 ] as const);
 const player = ref<string>("X");
+const isPlayerComputer = ref<boolean>(false);
+
 const playerXScore = ref<number[]>([]);
 const playerYScore = ref<number[]>([]);
 
 const checkWinners = (playerArray: number[]) => {
   return winnerLines.some((elem) =>
-    elem.every((item) => playerArray.includes(item))
+    elem.every((item) => playerArray.includes(item)),
   );
+};
+
+const getComputerStep = () => {
+  const clearBoard = board.filter((elem) => !elem.player);
+  const randomClearCell =
+    clearBoard[Math.floor(Math.random() * clearBoard.length)];
+  return randomClearCell;
 };
 
 const handleCellClickOnItem = (elem: { player: string; index: number }) => {
   const cell = board[elem.index];
   if (!cell || cell.player !== null) return;
+
+  // Ход игрока
   cell.player = player.value;
   if (player.value === "X") {
     playerXScore.value.push(elem.index);
@@ -50,6 +61,25 @@ const handleCellClickOnItem = (elem: { player: string; index: number }) => {
     playerYScore.value.push(elem.index);
   }
   player.value = player.value === "X" ? "O" : "X";
+
+  // Ход компьютера
+  if (isPlayerComputer.value && !winner.value && !isDraw.value) {
+    setTimeout(() => {
+      const randomClearCell = getComputerStep();
+      if (randomClearCell) {
+        const computerCell = board[randomClearCell.index];
+        if (computerCell) {
+          computerCell.player = player.value;
+          if (player.value === "X") {
+            playerXScore.value.push(computerCell.index);
+          } else {
+            playerYScore.value.push(computerCell.index);
+          }
+        }
+        player.value = player.value === "X" ? "O" : "X";
+      }
+    }, 100);
+  }
 };
 
 const resetGame = () => {
@@ -81,11 +111,14 @@ const isDraw = computed(() => {
       @clickeditem="handleCellClickOnItem"
     />
   </div>
-  <div>
+  <div class="controlBox">
     <h4 v-if="winner" style="color: green">Победил: {{ winner }}</h4>
     <h4 v-else-if="isDraw" style="color: orange">Ничья!</h4>
     <h4 v-else>Текущий игрок: {{ player }}</h4>
-
+    <div class="smallContainer">
+      <p>Играть с компьютером</p>
+      <input type="checkbox" v-model="isPlayerComputer" />
+    </div>
     <button @click="resetGame">Сбросить игру</button>
   </div>
 </template>
@@ -93,8 +126,17 @@ const isDraw = computed(() => {
 <style lang="css" scoped>
 .container {
   display: grid;
-  grid-template-columns: repeat(3, 1fr); 
-  max-width: 1200px; 
+  grid-template-columns: repeat(3, 1fr);
+  max-width: 1200px;
   margin: 0 auto;
+}
+
+.controlBox {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+.smallContainer {
+  display: flex;
 }
 </style>
--- src/components/SquareBoard.vue
+++ src/components/SquareBoard.vue
@@ -35,7 +35,6 @@ const toggleSymbol = () => {
 
 <style lang="css" scoped>
 .containerBoard {
-  width: 50px;
   height: 50px;
   border: 1px solid black;
   display: flex;
